9a471b634a8bbbc67cb932d34ba5bc45ed3679d7
Evelyn-F <144710767+Evelyn-F@users.noreply.github.com>
2023-09-20 09:31:01 -0700

    changed spacing

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
 	public static void main(String[] args) {
-      String name = "Bob Dobalina"; 
-      double score = 9.0;
-      score++;
-      System.out.println("Mr." + name + " has a score of " + (int) (score * score) );
+		String name = "Bob Dobalina"; 
+		double score = 9.0;
+		score++;
+		System.out.println("Mr." + name + " has a score of " + (int) (score * score) );
 	}
 }
